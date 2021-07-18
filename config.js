c456c8d0357010f5d6f9a647138741f5d81799bd
tientranhpnd <75087398+tientranhpnd@users.noreply.github.com>
2021-07-18 22:27:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'Bé My có biết là :',
-    introDesc: `Trái đất vốn lạ thường</br>
-    Mà sao em cứ đi nhầm đường\n
-    Lạc vào tim anh lẻ loi\n
+    introDesc: `Trái đất vốn lạ thường<br>
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
