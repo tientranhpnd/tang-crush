e4deb195f9f7a02405cfd9790490b86daf16cfa5
tientranhpnd <75087398+tientranhpnd@users.noreply.github.com>
2021-08-17 16:39:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Bé Thanhcó biết là :',
+    introTitle: 'Bé Thanh hâm, em có biết là :',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
